2e4ffc6f1ba3b9a911d49ac2b085beaf206b9717
Thomas Smith <thomasfn@fortfn.co.uk>
2021-03-04 21:06:42 +0000

    Updated modkit URL.

--- fetch-eco-reference-assemblies.sh
+++ fetch-eco-reference-assemblies.sh
@@ -4,7 +4,7 @@ MODKIT_VERSION="0.9.2.4-beta"
 MODKIT_FILENAME="EcoModKit_v$MODKIT_VERSION.zip"
 
 mkdir -p ./eco-dlls
-wget "https://s3-us-west-2.amazonaws.com/eco-releases/$MODKIT_FILENAME"
+wget "https://play.eco/s3/eco-releases/$MODKIT_FILENAME"
 unzip -o $MODKIT_FILENAME -d ./tmp
 cp ./tmp/ReferenceAssemblies/*.dll ./eco-dlls
 rm -r ./tmp
