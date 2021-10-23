37c8450bccdcb795f546b5efaa0304f657e5c652
Thomas Smith <thomasfn@fortfn.co.uk>
2021-10-23 21:16:21 +0100

    Added support for migrations.

---
+++ EcoCivicsImportExportMod/Migrations/ICivicsImpExpMigratorV1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json.Linq;
+
+namespace Eco.Mods.CivicsImpExp
+{
+    public interface ICivicsImpExpMigratorV1
+    {
+        bool ShouldMigrate(JObject obj);
+
+        bool ApplyMigration(JObject obj, IList<string> outMigrationReport);
+    }
+}
---
+++ EcoCivicsImportExportMod/Migrations/MigratorV1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Newtonsoft.Json.Linq;
+
+namespace Eco.Mods.CivicsImpExp.Migrations
+{
+    using Shared.Utils;
+
+    public class MigratorV1 : ICivicsImpExpMigratorV1
+    {
+        private static IEnumerable<ICivicsImpExpMigratorV1> InternalMigrators
+            => typeof(ICivicsImpExpMigratorV1).ConcreteTypesWithInteface()
+                .Except(new Type[] { typeof(MigratorV1), typeof(ExternalMigratorV1) })
+                .Select(t => Activator.CreateInstance(t) as ICivicsImpExpMigratorV1);
+
+        private static IEnumerable<ICivicsImpExpMigratorV1> ExternalMigrators
+            => AppDomain.CurrentDomain.GetAssemblies()
+                .FilteredAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.GetInterface("ICivicsImpExpMigratorV1") != null && !t.IsAssignableTo(typeof(ICivicsImpExpMigratorV1)))
+                .Select(t => new ExternalMigratorV1(Activator.CreateInstance(t)));
+
+        private readonly IEnumerable<ICivicsImpExpMigratorV1> allMigrators = ExternalMigrators.Concat(InternalMigrators);
+
+        public bool ShouldMigrate(JObject obj)
+        {
+            foreach (var migrator in allMigrators)
+            {
+                if (migrator.ShouldMigrate(obj))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool ApplyMigration(JObject obj, IList<string> outMigrationReport)
+        {
+            bool didMigrate = false;
+            foreach (var migrator in allMigrators)
+            {
+                if (migrator.ShouldMigrate(obj))
+                {
+                    didMigrate |= migrator.ApplyMigration(obj, outMigrationReport);
+                }
+            }
+            return didMigrate;
+        }
+
+        
+    }
+}
