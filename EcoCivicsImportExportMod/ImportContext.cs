489c0b2f21a51c27d7796680b386d7e8ef41106a
Thomas Smith <thomasfn@fortfn.co.uk>
2022-02-18 13:49:45 +0000

    Fixed some (de)serialisation issues concerning GamePickerList and GameValueContext.

--- EcoCivicsImportExportMod/ImportContext.cs
+++ EcoCivicsImportExportMod/ImportContext.cs
@@ -359,11 +359,9 @@ namespace Eco.Mods.CivicsImpExp
             }
             var gameValueContext = Activator.CreateInstance(gameValueContextType) as IGameValueContext;
             string contextName = obj.Value<string>("contextName");
-            gameValueContextType.GetProperty("ContextName", BindingFlags.Public | BindingFlags.Instance).SetValue(gameValueContext, contextName, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
-            string titleBacking = obj.Value<string>("titleBacking");
-            gameValueContextType.GetProperty("TitleBacking", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(gameValueContext, titleBacking, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
+            gameValueContextType.GetProperty("Name", BindingFlags.Public | BindingFlags.Instance).SetValue(gameValueContext, contextName, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
             string tooltip = obj.Value<string>("tooltip");
-            gameValueContextType.GetProperty("Tooltip", BindingFlags.Public | BindingFlags.Instance).SetValue(gameValueContext, tooltip, BindingFlags.Public | BindingFlags.Instance, null, null, null);
+            gameValueContextType.GetProperty("ContextDescription", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(gameValueContext, tooltip, BindingFlags.Public | BindingFlags.Instance, null, null, null);
             (gameValueContext as IController).Changed("Title");
             return gameValueContext;
         }
@@ -388,15 +386,29 @@ namespace Eco.Mods.CivicsImpExp
 
         private GamePickerList DeserialiseGamePickerList(JObject obj, Type expectedType)
         {
-            if (!expectedType.IsAssignableFrom(typeof(GamePickerList)))
+            GamePickerList gamePickerList;
+            if (expectedType.IsConstructedGenericType)
+            {
+                Type innerType = expectedType.GetGenericArguments()[0];
+                Type gamePickerListType = typeof(GamePickerList<>).MakeGenericType(innerType);
+                if (!expectedType.IsAssignableFrom(gamePickerListType))
+                {
+                    throw new InvalidOperationException($"Can't deserialise a '{gamePickerListType.FullName}' into a '{expectedType.FullName}'");
+                }
+                gamePickerList = Activator.CreateInstance(gamePickerListType, new object[] { null }) as GamePickerList;
+            }
+            else if (expectedType.IsAssignableFrom(typeof(GamePickerList)))
+            {
+                gamePickerList = new GamePickerList();
+                JObject mustDeriveType = obj.Value<JObject>("mustDeriveType");
+                if (mustDeriveType != null)
+                {
+                    gamePickerList.MustDeriveType = DeserialiseValueAsType(mustDeriveType, typeof(Type)) as Type;
+                }
+            }
+            else
             {
                 throw new InvalidOperationException($"Can't deserialise a GamePickerList into a '{expectedType.FullName}'");
-            }
-            var gamePickerList = new GamePickerList();
-            JObject mustDeriveType = obj.Value<JObject>("mustDeriveType");
-            if (mustDeriveType != null)
-            {
-                gamePickerList.MustDeriveType = DeserialiseValueAsType(mustDeriveType, typeof(Type)) as Type;
             }
             string requiredTag = obj.Value<string>("requiredTag");
             if (!string.IsNullOrEmpty(requiredTag))
