997bbf6f63627dcc3450f0cf6df0d7ffa334fd95
Thomas Smith <thomasfn@fortfn.co.uk>
2023-12-14 15:47:21 +0000

    Updated to Eco release build 532.

--- fetch-eco-reference-assemblies.sh
+++ fetch-eco-reference-assemblies.sh
@@ -3,8 +3,7 @@
 MODKIT_FILENAME="EcoModKit_v$MODKIT_VERSION.zip"
 
 mkdir -p ./eco-dlls
-#wget "https://play.eco/s3/$ECO_BRANCH/$MODKIT_FILENAME"
-wget https://eco-releases-staging-eu.s3.eu-central-1.amazonaws.com/$MODKIT_FILENAME
+wget "https://play.eco/s3/$ECO_BRANCH/$MODKIT_FILENAME"
 unzip -o $MODKIT_FILENAME -d ./tmp
 cp ./tmp/ReferenceAssemblies/*.dll ./eco-dlls
 rm -r ./tmp
